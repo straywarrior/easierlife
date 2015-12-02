4b32337746110462442226171f081588869b1dea
fzt <fengzhoutian@hotmail.com>
2015-12-02 10:54:54 +0800

    Add vim-template templates

---
+++ Vim/vim-template/templates/=template=.c
@@ -0,0 +1,14 @@
+/*
+ * %FFILE%
+ * @Author:     %USER%
+ * @Date:       %DATE%
+ * @Email:      %MAIL%
+ * Copyright (C) %YEAR% %USER%
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+#include "%FILE%.h"
+
+
+%HERE%
