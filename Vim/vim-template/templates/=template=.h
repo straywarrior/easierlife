dd3354ea7766ceacb5a240f3228d3db8943e0910
fzt <fengzhoutian@hotmail.com>
2015-12-02 10:54:54 +0800

    Add vim-template templates

---
+++ Vim/vim-template/templates/=template=.h
@@ -0,0 +1,16 @@
+/*
+ * %FILE%
+ * @Author:     %USER%
+ * @Date:       %DATE%
+ * @Email:      %MAIL%
+ * Copyright (C) %YEAR% %USER%
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+#ifndef %GUARD%
+#define %GUARD% value
+
+%HERE%
+
+#endif /* !%GUARD% */
