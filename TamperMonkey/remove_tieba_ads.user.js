fc39fd1d0644b5983d6fb4f40ee6e3b1b8e8ef3f
Feng Zhoutian <fengzhoutian@hotmail.com>
2016-10-12 23:28:37 +0800

    Add TamperMonkey script: remove_tieba_ads.user.js

---
+++ TamperMonkey/remove_tieba_ads.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         See Goodbye to Tieba Ads
+// @namespace    strayscript
+// @version      0.1
+// @description  Remove ads from the pages of Tieba. May add other ads later.
+// @author       StrayWarrior
+// @include      tieba.baidu.com/*
+// @grant        unsafeWindow
+// @require      https://cdn.bootcss.com/jquery/3.1.0/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Remove long recommendations in the post content
+    $("div.thread-recommend").each(function(){
+        $(this).css("display", "none");
+    });
+})();
