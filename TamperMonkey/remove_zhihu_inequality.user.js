bd9151587eba804f190ae6ec91cfb56d127d90fc
StrayWarrior <i@straywarrior.com>
2018-04-28 16:05:53 +0800

    Add TamperMonkey script: remove_zhihu_inequality.user.js

---
+++ TamperMonkey/remove_zhihu_inequality.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Remove Zhihu Inequality Clauses
+// @namespace    strayscript
+// @version      0.1
+// @description  Let the inequality clauses about privacy go away!
+// @author       StrayWarrior
+// @include      *
+// @grant        unsafeWindow
+// @require      https://cdn.bootcss.com/jquery/3.1.0/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $("html").css("overflow", "visible");
+    $("div.PrivacyConfirm-modal").parentNode.remove();
+
+})();
