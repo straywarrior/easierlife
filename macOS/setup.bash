0b4bd3918af37448c1ca5aa869cc9affa89731a8
StrayWarrior <i@straywarrior.com>
2018-05-16 14:15:14 +0800

    Add macOS/setup.bash for one-step setup of macOS

---
+++ macOS/setup.bash
@@ -0,0 +1,45 @@
+#! /bin/bash
+#
+# setup.bash
+# Copyright (C) 2018 StrayWarrior <i@straywarrior.com>
+#
+
+install_brew() {
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    # Use faster mirror for formula
+    cd "$(brew --repo)"
+    git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+    cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+    git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+    brew update
+
+    # Use faster mirror for bottle
+    echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles' >> ~/.bash_profile
+}
+
+install_fish() {
+    brew install fish
+
+    # Install Oh My Fish
+    curl -L https://get.oh-my.fish | fish
+    fish -c "omf install agnoster; omf theme agnoster"
+
+    # Install autojump
+    brew install autojump
+}
+
+configure_fish() {
+    cat >> ~/.config/fish/config.fish <<-EOF 
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+set HOMEBREW_BOTTLE_DOMAIN https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+EOF
+}
+
+main() {
+    install_brew
+    install_fish
+    configure_fish
+}
+
+main
