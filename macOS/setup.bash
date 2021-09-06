304c0c51300f779b7a3b2c51c6093f489a0b40b6
StrayWarrior <i@straywarrior.com>
2021-09-06 20:32:08 +0800

    Update macOS/setup.bash

--- macOS/setup.bash
+++ macOS/setup.bash
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash -e
 #
 # setup.bash
 # Copyright (C) 2018 StrayWarrior <i@straywarrior.com>
@@ -13,12 +13,16 @@ install_git() {
         echo "Git is not installed. Install Git from brew"
         brew install git
     fi
-    git clone https://github.com/straywarrior/easierlife.git /tmp/easierlife
+}
+
+configure_git() {
     if [ -f ~/.gitconfig ]; then
         mv -v ~/.gitconfig ~/.gitconfig.backup
     fi
-    cp -v /tmp/easierlife/Git/.gitconfig ~/.gitconfig
+    curl "https://raw.githubusercontent.com/straywarrior/easierlife/master/Git/.gitconfig" -o ~/.gitconfig
+    echo ".gitconfig saved to user's home."
 }
+
 
 configure_brew() {
     # Use faster mirror for formula
@@ -49,14 +53,33 @@ configure_fish() {
 set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
 set VIRTUAL_ENV_DISABLE_PROMPT false
 EOF
+
+    echo "Try to add fish shell as standard shell. Root privilege is required."
+    sudo bash -c "grep /usr/local/bin/fish /etc/shells || echo /usr/local/bin/fish >> /etc/shells"
+    chsh -s /usr/local/bin/fish
+}
+
+configure_vim() {
+    __WORKDIR=$PWD
+    cd $HOME
+    git clone https://github.com/straywarrior/vimfiles.git .vim
+    cd .vim
+    git submodule update --init
+    # patch vim plugins
+    cd bundle/neomru.vim
+    git apply $__WORKDIR/neomru.autoload.patch
+    echo "Vim plugin neomru patched."
+    cd $__WORKDIR
 }
 
 main() {
     install_brew
     install_git
+    configure_git
     configure_brew
     install_fish
     configure_fish
+    configure_vim
 }
 
 main
